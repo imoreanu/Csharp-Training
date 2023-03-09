53c7749f7080c8dd9b31e405eed7f30ebfc78e59
imoreanu <imoreanu@EN1210313>
2023-03-09 13:49:41 +0200

    Add project files.

---
+++ HomeWorkSession1/HomeWorkSession1/Program.cs
@@ -0,0 +1,31 @@
+﻿int BoxA, BoxB, BoxC, BoxD, BoxE, BoxF, BoxG, BoxH, BoxI;
+BoxA = 45;
+BoxB = 56;
+BoxC = BoxA + BoxB;
+BoxD = BoxA - BoxB;
+BoxE = BoxA * BoxB;
+BoxF = BoxA / BoxB;
+BoxG = BoxA % BoxB;
+BoxH = ++BoxA;
+BoxI = --BoxB;
+
+//Addition
+Console.WriteLine(BoxC);
+
+//Substraction
+Console.WriteLine(BoxD);
+
+//Multiplication
+Console.WriteLine(BoxE);
+
+//Division
+Console.WriteLine(BoxF);
+
+//Modulus
+Console.WriteLine(BoxF);
+
+//Increment
+Console.WriteLine(BoxH);
+
+//Decrement
+Console.WriteLine(BoxI);
